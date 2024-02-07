acf1cb80956ed96ef64bc23d98d421a6c6111c62
Vladislav <Ledovskikh.vladislav@yandex.ru>
2024-02-07 12:25:52 +0300

    ver 0.1

--- bot.py
+++ bot.py
@@ -8,21 +8,18 @@ bot = telebot.TeleBot('6842550234:AAEMaf-fHRaMudvrE6lPaWCtAqVe2-wTKf4')
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("👋 Поздороваться")
-    btn2 = types.KeyboardButton("❓ Задать вопрос")
-    markup.add(btn1, btn2)
-    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я тестовый бот для твоей статьи для habr.com".format(message.from_user), reply_markup=markup)
+    markup.add(btn1)
+    time.sleep(1)
+    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я чат-бот Колледжа связи №54 ОП-5".format(message.from_user), reply_markup=markup)
+    time.sleep(1)
+    bot.send_message(message.chat.id, text="Здесь ты сможешь заполнить анкету для поступления, узнать про специальности нашего подразделения, и еще что - то будет делать (пока хз))".format(message.from_user), reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+def func(message):
+    if(message.text == "👋 Поздороваться"):
+        bot.send_message(message.chat.id, text="Привеет..!)")
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    elif message.text == "1":
-        time.sleep(1)
-        bot.send_message(message.from_user.id, "Привет! Я чат-бот Колледжа связи №54 ОП-5")
-        time.sleep(1)
-        bot.send_message(message.from_user.id, "Здесь ты сможешь заполнить анкету для поступления, узнать про специальности нашего подразделения, и еще что - то будет делать (пока хз))")
+        bot.send_message(message.from_user.id, "Пока ничего нету")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
         
