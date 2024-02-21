a13acfafef97fbb1fb208829f55ee0f773c7e31b
Владислав Ледовских <ledovskikh.vladislav@yandex.ru>
2024-02-21 16:07:49 +0300

    Анкета

--- bot.py
+++ bot.py
@@ -4,13 +4,16 @@ import time;
 
 bot = telebot.TeleBot('6842550234:AAEMaf-fHRaMudvrE6lPaWCtAqVe2-wTKf4')
 
+name = ''
+age = 0
+surname = ''
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("👋 Поздороваться")
-    btn2 = types.KeyboradButton("Регистрация ")
-    markup.add(btn1)
+    btn2 = types.KeyboardButton("Регистрация ")
+    markup.add(btn1, btn2)
     time.sleep(1)
     bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я чат-бот Колледжа связи №54 ОП-5".format(message.from_user), reply_markup=markup)
     time.sleep(1)
@@ -34,35 +37,30 @@ def func(message):
         btn1 = types.KeyboardButton("👋 Поздороваться")
         markup.add(btn1)
         bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+    elif (message.text == "Заполнить анкету"):
+        bot.send_message(message.from_user.id, "Как тебя зовут?")
+        bot.register_next_step_handler(message, get_name); 
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
-name = '';
-age = 0;
-surname = '';
-@bot.message_handler(content_types=['text'])
-def start(message):
-    if message.text == '/reg':
-        bot.send_message(message.from_user.id, "Как тебя зовут?")
-        bot.register_next_step_handler(message,get_name); #следующий шаг функция get_name
-    else:
-        bot.send_message(message.from_user.id, "Напиши /reg");
+        
 def get_name(message):
-    global name;
-    name = message.text;
-    bot.send_message(message.from_user.id, "Какая у тебя фамлиия?");
-    bot.register_next_step_handler(message.get_surname);
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, "Какая у тебя фамилия?")
+    bot.register_next_step_handler(message, get_surname)
 def get_surname(message):
-    global surname;
-    surname = message.text;
-    bot.send_message("сколько тебе лет? ");
-    bot.register_next_step_handler(message, get_age);
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id, "сколько тебе лет? ")
+    bot.register_next_step_handler(message, get_age)
 def get_age(message):
-    global age;
-    while age ==0:
+    global age
+    while age == 0:
         try:
             age = int(message.text)
         except Exception:
-            bot.sende_message(message.from_user.id, "Цифрами пожалуйста!");
-        bot.send_message(message.from_user.id, "Тебе" + str(age) + " лет, тебя зовут " + name + " " + surname + "?")
+            bot.send_message(message.from_user.id, "Цифрами пожалуйста!")
+        bot.send_message(message.from_user.id, "Тебе " + str(age) + " лет, тебя зовут " + name + " " + surname + "?")
+        
+
 bot.polling(none_stop=True, interval=0)
