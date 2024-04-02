63b10d15e84d2cf0b46a84627d1937ec00d5b3bd
Apillene <ikuplinov14@gmail.com>
2024-04-02 14:47:31 +0300

    Добавление списка направлений в файл bot2.py и создание нового файла для подготовки к ЕГЭ

--- bot2.py
+++ bot2.py
@@ -7,23 +7,30 @@ bot = telebot.TeleBot ('7074490969:AAG6NZ22f0TOLTVhP_pKdXV2gxleK-KdEhs') # то�
 @bot.message_handler(commands=['start'])
 
 def start(message):
-    name = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
-    bot.send_message(message.chat.id, name, parse_mode='html')
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    btn1 = types.KeyboardButton("Начать!")
+    name = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u>. Это бот колледжа связи №54. Данный бот предоставляет возможность пройти профориентацию и узнать какое направление подойдет именно тебе!</b>'
+    markup.add(btn1)
+    bot.send_message(message.chat.id, name, parse_mode='html', reply_markup=markup)
 
-@bot.message_handler(commands=['website'])
-def website(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Посетить сайт", url="https://google.com"))
-    bot.send_message(message.chat.id, "перейти на сайт", parse_mode='html', reply_markup=markup)
+@bot.message_handler(content_types=['text'])
+def message (message):
+    if  message.text == ('Начать!'):
+        markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+        btn1 = types.KeyboardButton("Информационные системы и программирование")
+        btn2 = types.KeyboardButton("Обеспечение информационной безопасности телекоммуникационных систем")
+        btn3 = types.KeyboardButton("Обеспечение информационной безопасности автоматизированных систем")
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.chat.id, text ="Выбери специальность, которая тебя привлекает: ", reply_markup=markup)
+    elif message.text == "Информационные системы и программирование":
+        bot.send_message(message.chat.id, text='Вы выбрали информационные системы и программирование')
+    elif message.text == "Обеспечение информационной безопасности телекоммуникационных систем":
+        bot.send_message(message.chat.id, text="Вы выбрали обеспечение информационной безопасности телекоммуникационных систем")
+    elif message.text == "Обеспечение информационной безопасности автоматизированных систем":
+        bot.send_message(message.chat.id, text = "Вы выбрали обеспечение информационной безопасности автоматизированных систем")
+    else:
+        bot.send_message(message.chat.id, text= "Вы ничего не выбрали, сделайте выбор!")
 
-    
-@bot.message_handler(commands=['help'])
-def website(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    website = types.KeyboardButton("Сайт гугл")
-    start = types.KeyboardButton("Старт")
-    markup.add(website,start)
-    bot.send_message(message.chat.id, "перейти на сайт", parse_mode='html', reply_markup=markup)
 
 
 bot.polling(none_stop=True)
